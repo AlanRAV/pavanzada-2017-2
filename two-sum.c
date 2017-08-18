f507f531e8ab3c4da275ab23ddc2127d15a73f6a
AlanRAV <alan_alvarez1@hotmail.com>
2017-08-17 18:36:47 -0600

    Makefile

--- two-sum.c
+++ two-sum.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include "two-sum.h"
+
 
 int *getIndexes (int *nums, int len, int target){
 	int i, j;
@@ -26,17 +27,4 @@ int *getIndexes (int *nums, int len, int target){
 
 	}
 	return NULL;
-}
-int main (){
-	int nums [] = {2, 7, 11, 15};
-	int target = 9;
-	int len =4;
-	int *result = getIndexes (nums, len, target);
-		printf(" ");
-	if (result != NULL && (nums[result[0]] + nums[result[1]] == target)){
-		printf("Works\n");
-	} else{
-		printf(":( \n");
-
-	}
 }
