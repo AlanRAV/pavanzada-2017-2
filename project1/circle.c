3ac1286f35479c8b6e1893725fdd45b19c80c77f
AlanRAV <alan_alvarez1@hotmail.com>
2017-09-06 21:50:04 -0600

    Project 1

---
+++ project1/circle.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "circle.h"
+
+
+bool judgeCircle(char* moves) {
+	int ret[2]={0};
+	while(*moves++){
+		switch(*(moves-1)){
+			case 'U':ret[0]+=1;
+			break;
+			case 'D':ret[0]-=1;
+			break;
+			case 'L':ret[1]+=1;
+			break;
+			case 'R':ret[1]-=1;
+			break;
+		}
+	}
+	return ret[0]==0&&ret[1]==0;
+}
+
