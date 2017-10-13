2be2c6ce81352b81fe8c1ed0a10eb3f4705863b3
AlanRAV <alan_alvarez1@hotmail.com>
2017-10-12 23:42:32 -0600

    Kata4 ajustes

--- kata4/kata4.c
+++ kata4/kata4.c
@@ -2,22 +2,20 @@
 #include <stdio.h>
 #include "kata4.h"
 
-/** island_perimeter 
-  * Receives a bidimensional array of 0's and 1's that represent
-  * an islands area, where there's a 1, there's land, and where
-  * there's a  0, there's water. The array is always rectangular.
-  * Returns the perimeter of the island.
+/** Kata4 
+  * It takes an array od two dimensions representing the island land, the number of 
+  * rows and the number of columns to calculate the perimeter of the island land 
   * @param:
   *   island: the bidimensional array that represents the island.
-  *   row_size: the amount of rows in island.
-  *   col_size: the amount of columns in island.
+  *   rowSize: the amount of rows in island.
+  *   colSize: the amount of columns in island.
   * @return:
   *   int: perimeter of the island.
  */
-int kata4(int ** island, int row_size, int col_size) {
+int kata4(int ** island, int rowSize, int colSize) {
 	int perimeter = 0, i, j, tempPerimeter;
-	for (i = 0; i < row_size; i++) {
-		for (j = 0; j < col_size; j++) {
+	for (i = 0; i < rowSize; i++) {
+		for (j = 0; j < colSize; j++) {
 			tempPerimeter = 4;
 			if (island[i][j] == 0) {
 				tempPerimeter = 0;
@@ -26,13 +24,13 @@ int kata4(int ** island, int row_size, int col_size) {
 				if (i != 0) {
 					tempPerimeter -= island[i - 1][j];
 				}
-				if (i != row_size - 1) {
+				if (i != rowSize - 1) {
 					tempPerimeter -= island[i + 1][j];
 				}
 				if (j != 0) {
 					tempPerimeter -= island[i][j - 1];
 				}
-				if (j != col_size - 1) {
+				if (j != colSize - 1) {
 					tempPerimeter -= island[i][j + 1];
 				}
 			}
